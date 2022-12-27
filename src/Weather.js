977b61daa939c801b89028acf81c616662826f4b
Samin <86338112+samxn@users.noreply.github.com>
2022-12-26 23:29:28 -0500

    Modified alert

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default function Weather(props) {
   function handleResponse(response) {
-    alert(`${response.data.main.temp}`);
+    alert(`weather in ${props.city} is ${response.data.main.temp}`);
   }
   let apiKey = "094780c710fa4efd669f0df8c3991927";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
